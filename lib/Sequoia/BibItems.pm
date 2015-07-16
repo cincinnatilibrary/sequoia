6e17ca9f4d0dc3c2ec1c9f845b636cf475cef9c1
Dave Menninger <dave.menninger@gmail.com>
2015-07-16 16:06:53 -0400

    strip leading capital B or lowercase b

--- lib/Sequoia/BibItems.pm
+++ lib/Sequoia/BibItems.pm
@@ -1,8 +1,8 @@
 package Sequoia::BibItems;
-  
+
 use strict;
 use warnings;
- 
+
 use strict;
 use warnings;
 
@@ -21,8 +21,8 @@ sub items_for_bib_number
 	$sql_query .= "sierra_view.bib_view.title ";
 	$sql_query .= "FROM sierra_view.bib_view, sierra_view.item_view, sierra_view.bib_record_item_record_link ";
 	$sql_query .= "WHERE sierra_view.bib_view.id = sierra_view.bib_record_item_record_link.bib_record_id ";
-	$sql_query .= "AND sierra_view.item_view.id = sierra_view.bib_record_item_record_link.item_record_id ";	
-	
+	$sql_query .= "AND sierra_view.item_view.id = sierra_view.bib_record_item_record_link.item_record_id ";
+
 	#validate/munge the bibnumber ( we want to get it with no 'b' and no check digit )
 	#possibly this validation should be done earlier
 	if ( $bibnumber =~ m{ \A (b[0-9]{7})([x0-9]) \z }xmsi )
@@ -41,22 +41,22 @@ sub items_for_bib_number
 		$bibnumber = '0';
 	}
 
-	$bibnumber =~ s/b//;  #remove the leading b
+	$bibnumber =~ s/^(b|B)//;  #remove the leading b or B
 	my $b_record_num = $bibnumber;
 	$sql_query .= "AND sierra_view.bib_view.record_num = ? "; #  . $b_record_num . " ";
-			
+
 	#if icode1 was included, include it in the sql
-	#TODO: input validation on this to make sure it's a number 
+	#TODO: input validation on this to make sure it's a number
 	my $using_icode1 = 0;
 	if ( looks_like_number($icode1) ){
 		$using_icode1 = 1;
 		$sql_query .= "AND sierra_view.item_view.icode1 = ? "; #'" . $icode1 . "' ";
 	}
-	
+
 	$sql_query .= ";";
 
 	my $sth = $dbh->prepare($sql_query);
-	
+
 	#pass right number of placeholders depending on icode1
 	( $using_icode1 eq 1 ) ? $sth->execute( $b_record_num, $icode1 ) : $sth->execute( $b_record_num );
 
@@ -65,14 +65,14 @@ sub items_for_bib_number
 		push @item_ids, $row->{'barcode'};
 		$title = $row->{'title'};
 	}
-	
+
 	$num_items_found = scalar ( @item_ids );
 
 	return json => {
 		$bibnumber => {
-		 barcodes => \@item_ids,
-		 title => $title,
-		 num_items_found => $num_items_found
+		barcodes => \@item_ids,
+		title => $title,
+		num_items_found => $num_items_found
 		}
 	};
 }
