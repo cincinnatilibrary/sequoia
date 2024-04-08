b49d56b5bc2069fa57e5d78a8edd22dea96efc37
Ray Voelker <ray.voelker@gmail.com>
2024-04-07 21:03:15 -0400

    updated barcode to include chpl.org text (#1)

--- run_sequoia.sh
+++ run_sequoia.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cd /home/plchuser/app/sequoia/
-/home/plchuser/app/sequoia/sequoia.pl daemon -m production -l http://127.0.0.1:8080
+
+cd "$(dirname "$0")"
+
+/usr/bin/cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
+
+/usr/bin/perl sequoia.pl daemon -m production -l http://127.0.0.1:8080
